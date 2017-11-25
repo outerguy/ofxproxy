8952173918cbaba941fbe1831061055f46e9e4d0
アウター・ガイ <mail@outerguy.com>
2017-11-25 15:23:17 +0900

    著作権表示を更新する

--- client.php
+++ client.php
@@ -1,7 +1,7 @@
 <?php
 /*
 client.php: JavaScriptを生成する
-Copyright (C) 2012-2015 OFFICE OUTERGUY. All rights reserved.
+Copyright (C) 2012-2017 OFFICE OUTERGUY. All rights reserved.
 mailto:contact@beatrek.com
 Dual-licensed under the GNU AGPLv3 and Beatrek Origin License.
 */
